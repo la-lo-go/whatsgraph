cfa72653a691cc7829229781314eed453be8162d
Lalo <gonzalo.alvalon@gmail.com>
2024-08-06 22:56:10 +0200

    Yes, i test in production

--- src/utils/ZipManager.ts
+++ src/utils/ZipManager.ts
@@ -9,12 +9,14 @@ class ZipManager {
 
   async loadZipContent(binaryStr: ArrayBuffer): Promise<JSZip.JSZipObject[]> {
     const zipContent = await this.zip.loadAsync(binaryStr);
+    console.info('Zip content:', zipContent);
     return Object.values(zipContent.files);
   }
 
   async getFirstTxtFileContent(files: JSZip.JSZipObject[]): Promise<string | null> {
     const firstTxtFile = files.find((zipFile) => zipFile.name.endsWith('.txt'));
     if (firstTxtFile) {
+      console.info('Found txt file in Zip:', firstTxtFile);
       return await firstTxtFile.async('text');
     }
     return null;
