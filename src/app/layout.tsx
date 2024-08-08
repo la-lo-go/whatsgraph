cfa45d46949aed22de4bc416ff136dbfea2453f9
Lalo <gonzalo.alvalon@gmail.com>
2024-08-08 21:23:38 +0200

    Update page title and description in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "WhatsGraph",
+  title: process.env.PRO ? "WhatsGraph" : "[DEV] WhatsGraph",
   description: "Easily visualize your WhatsApp conversations data",
 };
 
