5d06f0d1005c25021e1bee1214609fb778278004
Lalo <gonzalo.alvalon@gmail.com>
2024-08-28 19:05:09 +0200

    DELETE footer

--- src/components/charts/MessagesPerDayChart.tsx
+++ src/components/charts/MessagesPerDayChart.tsx
@@ -333,18 +333,6 @@ export default function MessagesPerDayChart({
           ))}
         </Tabs>
       </CardContent>
-      <CardFooter>
-        <div className="flex w-full items-start gap-2 text-sm">
-          <div className="grid gap-2">
-            <div className="flex items-center gap-2 font-medium leading-none">
-              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-            </div>
-            <div className="flex items-center gap-2 leading-none text-muted-foreground">
-              Showing total messages for each sender in the selected time range
-            </div>
-          </div>
-        </div>
-      </CardFooter>
     </Card>
   );
 }
